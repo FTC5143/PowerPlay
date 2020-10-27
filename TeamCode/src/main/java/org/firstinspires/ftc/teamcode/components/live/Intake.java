5508ac3969638761a58e9947b5ab4e9ba62e1c1e
robotics <robotics@xaviersaints.org>
2020-10-27 15:16:26 -0500

    intake component

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/live/Intake.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.components.live;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.components.Component;
+import org.firstinspires.ftc.teamcode.robots.Robot;
+
+public class Intake extends Component {
+
+    //// SERVOS ////
+    private CRServo front_intake;
+    private CRServo back_intake;
+    private CRServo front_lift;
+    private CRServo back_lift;
+
+    //// SENSORS ////
+
+    {
+        name = "Intake";
+    }
+
+    public Intake(Robot robot) {
+        super(robot);
+    }
+
+    @Override
+    public void registerHardware(HardwareMap hwmap) {
+        super.registerHardware(hwmap);
+
+        //// MOTORS ////
+        front_intake    = hwmap.get(CRServo.class, "front_intake");
+        back_intake     = hwmap.get(CRServo.class, "back_intake");
+        front_lift      = hwmap.get(CRServo.class, "front_lift");
+        back_lift       = hwmap.get(CRServo.class, "back_lift");
+    }
+
+    @Override
+    public void update(OpMode opmode) {
+        super.update(opmode);
+    }
+
+    @Override
+    public void updateTelemetry(Telemetry telemetry) {
+        super.updateTelemetry(telemetry);
+    }
+
+    @Override
+    public void startup() {
+        super.startup();
+
+        front_intake.setPower(1);
+        back_intake.setPower(1);
+        front_lift.setPower(1);
+        back_lift.setPower(1);
+    }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+
+
+    }
+}
