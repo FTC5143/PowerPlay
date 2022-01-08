74ad7b61c7cfbad14830243b4f2e634160e1ca8b
Isaac <qwertytrogi@gmail.com>
2022-01-08 16:48:47 -0600

    autonomous base

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/PlaceBlockParkSquareAuto.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.opmodes.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.constants.AutonomousConst;
+import org.firstinspires.ftc.teamcode.opmodes.LiveAutoBase;
+
+public class PlaceBlockParkSquareAuto extends LiveAutoBase {
+
+    int color = AutonomousConst.RED;
+    int side = AutonomousConst.LEFT;
+
+    @Override
+    public void on_init() {
+        if (side == AutonomousConst.LEFT) {
+            robot.drive_train.odo_reset(24, 0, 0);
+        } else if (side == AutonomousConst.RIGHT) {
+            robot.drive_train.odo_reset(-24, 0, 0);
+        }
+    }
+
+    @Override
+    public void on_start() {
+        robot.intake.cradle_intake();
+
+        robot.drive_train.odo_move(0, -20, 0, 0.5, -1, -1, 4);
+
+        robot.intake.cradle_lift();
+        robot.lift.elevate_to(4);
+
+        halt(2);
+
+        robot.intake.cradle_dump();
+
+        halt(2);
+
+        robot.intake.cradle_intake();
+        robot.lift.min_lift();
+
+        if (color == AutonomousConst.RED) {
+            robot.drive_train.odo_move(24 * 2, -24, 0, 1, -1, -1, 5);
+        } else if (color == AutonomousConst.BLUE) {
+            robot.drive_train.odo_move(-24 * 2, -24, 0, 1, -1, -1, 5);
+        }
+    }
+
+    @Override
+    public void on_stop() {
+
+    }
+}
