c6a0cb2bc909f45e368280d59560d42a2ba3c0d1
ND-Scott <114361671+ND-Scott@users.noreply.github.com>
2022-12-02 16:22:32 -0600

    updated 7778

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BlueCorners.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BlueCorners.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.opmodes.LiveAutoBase;
 
+// Two cone auto starting in a blue corner
 @Autonomous(name = "Blue Corners", group = "autonomous")
 public class BlueCorners extends LiveAutoBase {
 
@@ -22,11 +23,15 @@ public class BlueCorners extends LiveAutoBase {
 
     @Override
     public void on_start() {
+
+        // Drop first cone
         robot.lift.elevate_to(1);
         robot.drive_train.odo_move(16.5,6,0,0.4);
         sleep(250);
         robot.lift.open_claw();
         sleep(500);
+
+        // Grab second cone
         robot.drive_train.odo_move(16.5, 2, 0, 0.4);
         sleep(500);
         robot.lift.cone_level(4);
@@ -35,12 +40,16 @@ public class BlueCorners extends LiveAutoBase {
         robot.drive_train.odo_move(52,-28,3.142,0.4);
         sleep(500);
         robot.lift.close_claw();
+
+        // Place second cone
         robot.lift.elevate_to(3);
         robot.drive_train.odo_move(52,0,0,0.4);
         robot.drive_train.odo_move(66,18,0,0.4);
         sleep(250);
         robot.lift.open_claw();
         sleep(500);
+
+        // Move to park and reset robot
         robot.drive_train.odo_move(52,24,0,0.4);
 
         if (pattern == 2) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/RedCorners.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/RedCorners.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.opmodes.LiveAutoBase;
 
+// Two cone auto starting in a red corner
 @Autonomous(name = "Red Corners", group = "autonomous")
 public class RedCorners extends LiveAutoBase {
 
@@ -22,22 +23,30 @@ public class RedCorners extends LiveAutoBase {
 
     @Override
     public void on_start() {
+
+        // Drop first cone
         robot.lift.elevate_to(1);
         robot.drive_train.odo_move(46,0,0,0.4);
         robot.drive_train.odo_move(40.5,6,0,0.25);
         robot.lift.open_claw();
         sleep(500);
+
+        // Grab second cone from stack
         robot.drive_train.odo_move(52,0,0,0.4);
         robot.lift.cone_level(4);
         robot.drive_train.odo_move(52,28,0,0.4);
         robot.lift.close_claw();
         sleep(500);
+
+        // Place second cone
         robot.lift.elevate_to(3);
         robot.drive_train.odo_move(52,-24,0,0.4);
         robot.drive_train.odo_move(66,-18,0,0.4);
         sleep(250);
         robot.lift.open_claw();
         sleep(500);
+
+        // Move to park and reset robot
         robot.drive_train.odo_move(66, -20, 0, 0.4);
         robot.lift.elevate_to(0);
         robot.drive_train.odo_move(52,-24,0,0.4);
